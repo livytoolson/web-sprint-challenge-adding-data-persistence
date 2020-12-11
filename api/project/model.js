f6e6f593e8341a5c898609cff09ce855a9aa765f
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:32:24 -0800

    updated project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,18 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    getAll,
+    add
+}
+
+function getAll() {
+    return db('')
+}
+
+function add(project) {
+    return db('')
+    .insert(project)
+    .then((id) => {
+        return db('').where({ id }).first()
+    })
+}
