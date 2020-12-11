85072e15ea31ceccf4d7a80dc635d5dd6c28c9e8
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:22:35 -0800

    updated dbConfig.js

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,1 +1,11 @@
-// complete your db configuration
+const knex = require('knex');
+
+const config = require('../knexfile');
+
+const db = knex(
+    process.env.NODE_ENV === 'production'
+    ? config.production
+    : config.development
+);
+
+module.exports = db;
