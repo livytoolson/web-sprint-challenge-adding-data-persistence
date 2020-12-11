572b4655107ad8f2669aa95abc608f1ee475e318
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:59:35 -0800

    created projects table migration

---
+++ data/migrations/20201211074610_projects_table.js
@@ -0,0 +1,47 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('name').notNullable()
+        table.string('description', 256)
+        table.boolean('completed').notNullable().defaultTo(false)
+    })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('name', 128).unique().notNullable()
+        table.string('description', 256)
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('description', 256).notNullable()
+        table.string('notes', 256)
+        table.boolean('completed').notNullable().defaultTo(false)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id').inTable('projects')
+            .onDelete('RESTRICT').onUpdate('RESTRICT')
+    })
+    .createTable('project_resources', table => {
+        table.increments('project_resource_id')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id').inTable('projects')
+            .onDelete('RESTRICT').onUpdate('RESTRICT')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id').inTable('resources')
+            .onDelete('RESTRICT').onUpdate('RESTRICT')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
