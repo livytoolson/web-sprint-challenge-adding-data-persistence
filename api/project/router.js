547602d07605ef73f0941b9d9934b5bb101c7d6c
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:37:16 -0800

    updated project router

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,29 @@
-// build your `/api/projects` router here
+const express = require('express');
+
+const Projects = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.find()
+    .then(projects => {
+        res.json(projects);
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    });
+});
+
+router.post('/', (req, res) => {
+    const projectData = req.body
+
+    Projects.add(projectData)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    });
+});
+
+module.exports = router;
