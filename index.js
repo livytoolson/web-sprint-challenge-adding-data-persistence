abc9a17a49f30e1d4be19802f8b2df7c87756e68
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:23:44 -0800

    updated server.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./server');
+
+const PORT = process.env.PORT || 5000;
+
+server.listeng(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
