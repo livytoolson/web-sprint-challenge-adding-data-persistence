97910848b1c66d57feee52f429e29c8efac20bc2
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:20:57 -0800

    updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,20 @@
 module.exports = {
   development: {
-    // complete your knexfile
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/projects.db3'
+    },
     migrations: {
-      directory: "./data/migrations",
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
   },
 };
