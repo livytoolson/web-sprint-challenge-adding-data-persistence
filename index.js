995aabfaac60b28236638097b739daf0925c2c47
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 08:25:03 -0800

    server listening on port 5000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./server');
+const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 
-server.listeng(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
