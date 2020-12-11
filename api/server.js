bcb19ac8e504630c1416495b6e8a581933d9bbe6
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 07:27:52 -0800

    added ResourceRouter, TaskRouter to server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const ProjectRouter = require('./project/router');
+const ResourceRouter = require('./resource/router');
+const TaskRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', ProjectRouter)
+server.use('/api/projects', ProjectRouter);
+server.use('/api/resources', ResourceRouter);
+server.use('/api/tasks', TaskRouter);
 
 module.exports = server;
