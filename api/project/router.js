2e4a6ce0348b91ee7ebd633d2ced2edef9b60dce
Olivia Toolson <livytoolson@gmail.com>
2020-12-11 11:35:33 -0800

    testing boolean

--- api/project/router.js
+++ api/project/router.js
@@ -17,21 +17,45 @@ const validateProject =  (req, res, next) => {
 
 router.get('/', (req, res) => {
     Projects.getAll()
-    .then(projects => {
+      .then(projects => {
+        projects.forEach(project => {
+          project.completed = Boolean(project.completed)
+        })
         res.json(projects);
-    })
-    .catch(error => {
-        res.status(500).json({ message: error.message })
-    });
-});
+      })
+      .catch(e => {
+        res.status(500).json({ message: e.message });
+      })
+  })
 
-router.post('/', validateProject, async (req, res) => {
-  try {
-    const newProject = await Projects.add(req.body)
-    res.status(201).json(newProject)
-  } catch (error) {
-    res.status(500).json({ message: error })
-  }
+// router.get('/', (req, res) => {
+//     Projects.getAll()
+//     .then(projects => {
+//         res.json(projects);
+//     })
+//     .catch(error => {
+//         res.status(500).json({ message: error.message })
+//     });
+// });
+
+// router.post('/', validateProject, async (req, res) => {
+//   try {
+//     const newProject = await Projects.add(req.body)
+//     res.status(201).json(newProject)
+//   } catch (error) {
+//     res.status(500).json({ message: error })
+//   }
+// })
+
+router.post('/', validateProject, (req, res) => {
+  Projects.add(req.body)
+    .then(project => {
+      project[0].completed = Boolean(project[0].completed)
+      res.json(project);
+    })
+    .catch(e => {
+      res.status(500).json({ message: e.message });
+    })
 })
 
 module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -18,21 +18,45 @@ const validateResource =  (req, res, next) => {
 
 router.get('/', (req, res) => {
     Resources.getAll()
-    .then(projects => {
-        res.json(projects);
-    })
-    .catch(error => {
-        res.status(500).json({ message: error.message })
-    });
-});
+      .then(resources => {
+        resources.forEach(resource => {
+          resource.completed = Boolean(resource.completed)
+        })
+        res.json(resources);
+      })
+      .catch(e => {
+        res.status(500).json({ message: e.message });
+      })
+  })
 
-router.post('/', validateResource, async (req, res) => {
-  try {
-    const newResource = await Resources.add(req.body)
-    res.status(201).json(newResource)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-});
+// router.get('/', (req, res) => {
+//     Resources.getAll()
+//     .then(projects => {
+//         res.json(projects);
+//     })
+//     .catch(error => {
+//         res.status(500).json({ message: error.message })
+//     });
+// });
+
+// router.post('/', validateResource, async (req, res) => {
+//   try {
+//     const newResource = await Resources.add(req.body)
+//     res.status(201).json(newResource)
+//   } catch (error) {
+//     res.status(500).json({ message: error.message })
+//   }
+// });
+
+router.post('/', validateResource, (req, res) => {
+  Resources.add(req.body)
+    .then(resource => {
+      resource[0].completed = Boolean(resource[0].completed)
+      res.json(resource);
+    })
+    .catch(e => {
+      res.status(500).json({ message: e.message });
+    })
+})
 
 module.exports = router;
